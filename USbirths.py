ba4d52a526a501bafde064d866e3985853db8261
corrinaricker <corrinaricker@users.noreply.github.com>
2025-01-19 13:42:34 -0500

    more data cleaning of natality data

--- USbirths.py
+++ USbirths.py
@@ -10,51 +10,12 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-from sklearn.preprocessing import OrdinalEncoder
+from sklearn.preprocessing import LabelEncoder, OrdinalEncoder
 
 #%%load data
 data_natality = pd.read_csv('C:/Users/crick/Documents/ML practice/Natality2016-2023.txt',
                             sep='\t')
 data_income = pd.read_csv('C:/Users/crick/Documents/ML practice/MD_census_income.csv')
-
-
-#%%check for mising values
-rows = data_natality.shape[0]
-print('Counties missing ', 
-      data_natality['County of Residence'].isnull().sum(), 
-      '{:.1%}'.format(data_natality['County of Residence'].isnull().sum()/rows))
-print('Prenatal Visits missing ', 
-      data_natality['Number of Prenatal Visits'].isnull().sum(), 
-      '{:.1%}'.format(data_natality['Number of Prenatal Visits'].isnull().sum()/rows))
-print('Age of Mother missing ', 
-      data_natality['Age of Mother 9'].isnull().sum(), 
-      '{:.1%}'.format(data_natality['Age of Mother 9'].isnull().sum()/rows))
-print('Mother Education missing ', 
-      data_natality["Mother's Education"].isnull().sum(), 
-      '{:.1%}'.format(data_natality["Mother's Education"].isnull().sum()/rows))
-print('NICU Admission missing ', 
-      data_natality['NICU Admission'].isnull().sum(), 
-      '{:.1%}'.format(data_natality['NICU Admission'].isnull().sum()/rows))
-print('Births missing ', 
-      data_natality['Births'].isnull().sum(), 
-      '{:.1%}'.format(data_natality['Births'].isnull().sum()/rows))
-print('Gestational Age missing ', 
-      data_natality['Average OE Gestational Age (weeks)'].isnull().sum(), 
-      '{:.1%}'.format(data_natality['Average OE Gestational Age (weeks)'].isnull().sum()/rows))
-print('Birth Weight missing ', 
-      data_natality['Average Birth Weight (grams)'].isnull().sum(), 
-      '{:.1%}'.format(data_natality['Average Birth Weight (grams)'].isnull().sum()/rows))
-print('BMI missing ', 
-      data_natality['Average Pre-pregnancy BMI'].isnull().sum(), 
-      '{:.1%}'.format(data_natality['Average Pre-pregnancy BMI'].isnull().sum()/rows))
-print('Birth Intervale missing ', 
-      data_natality['Average Interval Since Last Live Birth (months)'].isnull().sum(), 
-      '{:.1%}'.format(data_natality['Average Interval Since Last Live Birth (months)'].isnull().sum()/rows))
-
-#all features have the same number missing/null and it's a very small
-#percentage of the total (less than 1%) so I will drop nans
-data_natality = data_natality.dropna()
-
 #%%clean data
 data_natality_notes = data_natality.pop('Notes').dropna()
 
@@ -90,11 +51,84 @@ data_income.loc[data_income['County'].str.startswith('Maryland'), 'County'] = 'M
 cols_to_drop = [col for col in data_income.columns if col.startswith('PERCENT')]
 data_income.drop(cols_to_drop, axis=1, inplace=True)
 
+#%%check for mising values
+rows = data_natality.shape[0]
+print('Counties missing ', 
+      data_natality['County of Residence'].isnull().sum(), 
+      '{:.1%}'.format(data_natality['County of Residence'].isnull().sum()/rows))
+print('Prenatal Visits missing ', 
+      data_natality['Number of Prenatal Visits'].isnull().sum(), 
+      '{:.1%}'.format(data_natality['Number of Prenatal Visits'].isnull().sum()/rows))
+print('Age of Mother missing ', 
+      data_natality['Age of Mother 9'].isnull().sum(), 
+      '{:.1%}'.format(data_natality['Age of Mother 9'].isnull().sum()/rows))
+print('Mother Education missing ', 
+      data_natality["Mother's Education"].isnull().sum(), 
+      '{:.1%}'.format(data_natality["Mother's Education"].isnull().sum()/rows))
+print('NICU Admission missing ', 
+      data_natality['NICU Admission'].isnull().sum(), 
+      '{:.1%}'.format(data_natality['NICU Admission'].isnull().sum()/rows))
+print('Births missing ', 
+      data_natality['Births'].isnull().sum(), 
+      '{:.1%}'.format(data_natality['Births'].isnull().sum()/rows))
+print('Gestational Age missing ', 
+      data_natality['Average OE Gestational Age (weeks)'].isnull().sum(), 
+      '{:.1%}'.format(data_natality['Average OE Gestational Age (weeks)'].isnull().sum()/rows))
+print('Birth Weight missing ', 
+      data_natality['Average Birth Weight (grams)'].isnull().sum(), 
+      '{:.1%}'.format(data_natality['Average Birth Weight (grams)'].isnull().sum()/rows))
+print('BMI missing ', 
+      data_natality['Average Pre-pregnancy BMI'].isnull().sum(), 
+      '{:.1%}'.format(data_natality['Average Pre-pregnancy BMI'].isnull().sum()/rows))
+print('Birth Interval missing ', 
+      data_natality['Average Interval Since Last Live Birth (months)'].isnull().sum(), 
+      '{:.1%}'.format(data_natality['Average Interval Since Last Live Birth (months)'].isnull().sum()/rows))
+#all features have the same number missing/null and it's a very small
+#percentage of the total (less than 1%) so I will drop nans
+data_natality = data_natality.dropna()
+#there are some "Not Applicable" values in the Prenatal Visits column
+#and in the Intervale Since Last Live Birth column
+#check to see how mandy
+print('Prenatal Visits labeled Not Applicable ', 
+      data_natality.loc[data_natality['Average Number of Prenatal Visits']=='Not Applicable']['Average Number of Prenatal Visits'].count(),
+      '{:.1%}'.format(data_natality.loc[data_natality['Average Number of Prenatal Visits']=='Not Applicable']['Average Number of Prenatal Visits'].count()/rows))
+print('Live Birth labeled Not Applicable ', 
+      data_natality.loc[data_natality['Average Interval Since Last Live Birth (months)']=='Not Applicable']['Average Interval Since Last Live Birth (months)'].count(),
+      '{:.1%}'.format(data_natality.loc[data_natality['Average Interval Since Last Live Birth (months)']=='Not Applicable']['Average Interval Since Last Live Birth (months)'].count()/rows))
+
+#5% and 1.2%, acceptable to drop
+data_natality = data_natality.loc[data_natality['Average Number of Prenatal Visits']!='Not Applicable']
+data_natality = data_natality.loc[data_natality['Average Interval Since Last Live Birth (months)']!='Not Applicable']
+#change the datatypes of these columns to floats
+data_natality['Average Number of Prenatal Visits'] = data_natality['Average Number of Prenatal Visits'].astype(float)
+data_natality['Average Interval Since Last Live Birth (months)'] = data_natality['Average Interval Since Last Live Birth (months)'].astype(float)
+
+
 #%%set up data for modelling
+#convet qualitative values to numeric
+#age of mother should be ordinal
+ord_encoder = OrdinalEncoder(categories=[['15-19', '20-24', '25-29', '30-34', 
+                                          '35-39', '40-44', '45-49']])
+data_natality['Age of Mother encoded'] = ord_encoder.fit_transform(data_natality[['Age of Mother 9 Code']])
+ 
+#get standard deviations to get low, avg, and high for
+#birth weight, interval since last live birth
+#apply BMI categories
 
 #%%EDA
 #data.info()
 describe = data_natality.describe()
+
+numerical_columns = data_natality.select_dtypes(include=["int64", "float64"]).columns
+
+plt.figure(figsize=(14, len(numerical_columns) * 3))
+for idx, feature in enumerate(numerical_columns, 1):
+    plt.subplot(len(numerical_columns), 2, idx)
+    sns.histplot(data_natality[feature], kde=True)
+    plt.title(f"{feature} | Skewness: {round(data_natality[feature].skew(), 2)}")
+
+plt.tight_layout()
+plt.show()
 
 '''to do:
     -add low, normal, high to birth weight 
