c8e7a770372ce5c500d71bc240d1fd277d18ade8
corrinaricker <corrinaricker@users.noreply.github.com>
2025-01-20 13:53:48 -0500

    added some EDA on natality data

--- USbirths.py
+++ USbirths.py
@@ -53,34 +53,34 @@ data_income.drop(cols_to_drop, axis=1, inplace=True)
 
 #%%check for mising values
 rows = data_natality.shape[0]
-print('Counties missing ', 
+print('Counties missing', 
       data_natality['County of Residence'].isnull().sum(), 
       '{:.1%}'.format(data_natality['County of Residence'].isnull().sum()/rows))
-print('Prenatal Visits missing ', 
+print('Prenatal Visits missing', 
       data_natality['Number of Prenatal Visits'].isnull().sum(), 
       '{:.1%}'.format(data_natality['Number of Prenatal Visits'].isnull().sum()/rows))
-print('Age of Mother missing ', 
+print('Age of Mother missing', 
       data_natality['Age of Mother 9'].isnull().sum(), 
       '{:.1%}'.format(data_natality['Age of Mother 9'].isnull().sum()/rows))
-print('Mother Education missing ', 
+print('Mother Education missing', 
       data_natality["Mother's Education"].isnull().sum(), 
       '{:.1%}'.format(data_natality["Mother's Education"].isnull().sum()/rows))
-print('NICU Admission missing ', 
+print('NICU Admission missing', 
       data_natality['NICU Admission'].isnull().sum(), 
       '{:.1%}'.format(data_natality['NICU Admission'].isnull().sum()/rows))
-print('Births missing ', 
+print('Births missing', 
       data_natality['Births'].isnull().sum(), 
       '{:.1%}'.format(data_natality['Births'].isnull().sum()/rows))
-print('Gestational Age missing ', 
+print('Gestational Age missing', 
       data_natality['Average OE Gestational Age (weeks)'].isnull().sum(), 
       '{:.1%}'.format(data_natality['Average OE Gestational Age (weeks)'].isnull().sum()/rows))
-print('Birth Weight missing ', 
+print('Birth Weight missing', 
       data_natality['Average Birth Weight (grams)'].isnull().sum(), 
       '{:.1%}'.format(data_natality['Average Birth Weight (grams)'].isnull().sum()/rows))
-print('BMI missing ', 
+print('BMI missing', 
       data_natality['Average Pre-pregnancy BMI'].isnull().sum(), 
       '{:.1%}'.format(data_natality['Average Pre-pregnancy BMI'].isnull().sum()/rows))
-print('Birth Interval missing ', 
+print('Birth Interval missing', 
       data_natality['Average Interval Since Last Live Birth (months)'].isnull().sum(), 
       '{:.1%}'.format(data_natality['Average Interval Since Last Live Birth (months)'].isnull().sum()/rows))
 #all features have the same number missing/null and it's a very small
@@ -89,10 +89,10 @@ data_natality = data_natality.dropna()
 #there are some "Not Applicable" values in the Prenatal Visits column
 #and in the Intervale Since Last Live Birth column
 #check to see how mandy
-print('Prenatal Visits labeled Not Applicable ', 
+print('Prenatal Visits labeled Not Applicable', 
       data_natality.loc[data_natality['Average Number of Prenatal Visits']=='Not Applicable']['Average Number of Prenatal Visits'].count(),
       '{:.1%}'.format(data_natality.loc[data_natality['Average Number of Prenatal Visits']=='Not Applicable']['Average Number of Prenatal Visits'].count()/rows))
-print('Live Birth labeled Not Applicable ', 
+print('Live Birth labeled Not Applicable', 
       data_natality.loc[data_natality['Average Interval Since Last Live Birth (months)']=='Not Applicable']['Average Interval Since Last Live Birth (months)'].count(),
       '{:.1%}'.format(data_natality.loc[data_natality['Average Interval Since Last Live Birth (months)']=='Not Applicable']['Average Interval Since Last Live Birth (months)'].count()/rows))
 
@@ -140,27 +140,82 @@ def interval_thresholds(i):
         return 2
 data_natality['Interval Category'] = data_natality['Average Interval Since Last Live Birth (months)'].apply(interval_thresholds)
 
+#add BMI categories following:
+#Underweight: BMI is less than 18.5
+#Normal weight: BMI is 18.5 to 24.9
+#Overweight: BMI is 25 to 29.9
+#Obese: BMI is 30 or more
+def BMI_thresholds(bmi):
+    if bmi < 18.5:
+        #underweight
+        return 1
+    elif bmi < 24.9:
+        #normal
+        return 2
+    elif bmi < 29.9:
+        #overweight
+        return 3
+    else:
+        #ovese
+        return 4
+data_natality['BMI Category'] = data_natality['Average Pre-pregnancy BMI'].apply(BMI_thresholds)
+
     
 
 #%%EDA
 #data.info()
 describe = data_natality.describe()
-
 numerical_columns = data_natality.select_dtypes(include=["int64", "float64"]).columns
 
+#check for outliers with histograms
 plt.figure(figsize=(14, len(numerical_columns) * 3))
 for idx, feature in enumerate(numerical_columns, 1):
     plt.subplot(len(numerical_columns), 2, idx)
     sns.histplot(data_natality[feature], kde=True)
     plt.title(f"{feature} | Skewness: {round(data_natality[feature].skew(), 2)}")
-
 plt.tight_layout()
 plt.show()
+#%%check for outliers with 3 standard deviations away from mean
 
-'''to do:
-    -add low, normal, high to birth weight 
-    -same for gestational age
-    -look up census data for MD for income, etc by county
-    -replace 99s with Nans in prenatal visits
-'''
+min_prenatal_visits = data_natality['Average Number of Prenatal Visits'].mean() - (3 * data_natality['Average Number of Prenatal Visits'].std())
+max_prenatal_visits = data_natality['Average Number of Prenatal Visits'].mean() + (3 * data_natality['Average Number of Prenatal Visits'].std())
+outliers_prenatal_visits = data_natality.loc[data_natality['Average Number of Prenatal Visits'] > max_prenatal_visits]
+print('prenatal visits outliers:',
+      len(outliers_prenatal_visits),
+      '{:.1%}'.format(len(outliers_prenatal_visits)/rows))
+
+min_age = data_natality['Average Age of Mother (years)'].mean() - (3 * data_natality['Average Age of Mother (years)'].std())
+max_age = data_natality['Average Age of Mother (years)'].mean() + (3 * data_natality['Average Age of Mother (years)'].std())
+outliers_age = data_natality.loc[data_natality['Average Age of Mother (years)'] > max_age]
+print('age outliers:',
+      len(outliers_age),
+      '{:.1%}'.format(len(outliers_age)/rows))
+
+min_birth_weight = data_natality['Average Birth Weight (grams)'].mean() - (3 * data_natality['Average Birth Weight (grams)'].std())
+max_birth_weight = data_natality['Average Birth Weight (grams)'].mean() + (3 * data_natality['Average Birth Weight (grams)'].std())
+outliers_birth_weight = data_natality.loc[data_natality['Average Birth Weight (grams)'] > max_birth_weight]
+print('birth weight outliers:',
+      len(outliers_birth_weight),
+      '{:.1%}'.format(len(outliers_birth_weight)/rows))
+
+min_gest_age = data_natality['Average OE Gestational Age (weeks)'].mean() - (3 * data_natality['Average OE Gestational Age (weeks)'].std())
+max_gest_age = data_natality['Average OE Gestational Age (weeks)'].mean() + (3 * data_natality['Average OE Gestational Age (weeks)'].std())
+outliers_gest_age = data_natality.loc[data_natality['Average OE Gestational Age (weeks)'] > max_gest_age]
+print('gestational age outliers:',
+      len(outliers_gest_age),
+      '{:.1%}'.format(len(outliers_gest_age)/rows))
+
+min_bmi = data_natality['Average Pre-pregnancy BMI'].mean() - (3 * data_natality['Average Pre-pregnancy BMI'].std())
+max_bmi = data_natality['Average Pre-pregnancy BMI'].mean() + (3 * data_natality['Average Pre-pregnancy BMI'].std())
+outliers_bmi = data_natality.loc[data_natality['Average Pre-pregnancy BMI'] > max_bmi]
+print('BMI outliers:',
+      len(outliers_bmi),
+      '{:.1%}'.format(len(outliers_bmi)/rows))
+
+min_inter = data_natality['Average Interval Since Last Live Birth (months)'].mean() - (3 * data_natality['Average Interval Since Last Live Birth (months)'].std())
+max_inter = data_natality['Average Interval Since Last Live Birth (months)'].mean() + (3 * data_natality['Average Interval Since Last Live Birth (months)'].std())
+outliers_inter = data_natality.loc[data_natality['Average Interval Since Last Live Birth (months)'] > max_inter]
+print('Interval Since Last Live Birth outliers:',
+      len(outliers_inter),
+      '{:.1%}'.format(len(outliers_inter)/rows))
 
